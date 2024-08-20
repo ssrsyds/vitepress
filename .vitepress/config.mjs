b565befe1b330e17273c150c8f9e9d8fae0b24a0
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2024-08-20 10:54:11 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -5,7 +5,7 @@ export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
-    base: "vitepress",
+    base: "/vitepress/",
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
